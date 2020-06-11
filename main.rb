66332e375e5a072e4f97db75ff70918442792589
lunglagael <lunglagael@gmail.com>
2020-06-11 20:46:08 +0100

    updates

--- main.rb
+++ main.rb
@@ -2,24 +2,24 @@ require './player.rb'
 COLORS = ["red","purple","teal","orange","brown","blue"]
 POSITION = [1,2,3,4]
 
-human = Player.new("human")
-computer = Player.new("computer")
+$human = Player.new("human")
+$computer = Player.new("computer")
 is_valid_input = false
 
-def human_codebreaker(human,computer)
+def human_codebreaker
     puts "------------- Guess the secretCode!--------------"
     stop = false
     tries = 0
     loop do
         puts "-------------Guess Again!-------------" unless tries === 0
-        human.secret_code = {}
-        human.guess_secret_code
+        $human.secret_code = {}
+        $human.guess_secret_code
         
         feedback = []
-        def matched?(color,human,computer)
+        def matched?(color)
             value = false
-            computer.secret_code.each do |position,col|
-                if col === human.secret_code[position]
+            $computer.secret_code.each do |position,col|
+                if col === $human.secret_code[position]
                     if col === color
                         value = true                
                     end
@@ -28,22 +28,22 @@ def human_codebreaker(human,computer)
             value
         end
 
-        human.secret_code.each do |position, color|
-            if computer.secret_code.values.count(color) > 1 
-                if color === computer.secret_code[position]
+        $human.secret_code.each do |position, color|
+            if $computer.secret_code.values.count(color) > 1 
+                if color === $computer.secret_code[position]
                     feedback.push("black")
                     next
                 else
-                    feedback.unshift("white") if human.secret_code.values.first(position).count(color) === 1 && matched?(color,human,computer) == false
+                    feedback.unshift("white") if $human.secret_code.values.first(position).count(color) === 1 && matched?(color) == false
                     next
                 end
             else
-                if color === computer.secret_code[position]
+                if color === $computer.secret_code[position]
                     feedback.push("black")
                     next
                 else
-                    if matched?(color,human,computer) == false && computer.secret_code.values.include?(color)
-                        feedback.unshift("white") if human.secret_code.values.first(position).count(color) === 1
+                    if matched?(color) == false && $computer.secret_code.values.include?(color)
+                        feedback.unshift("white") if $human.secret_code.values.first(position).count(color) === 1
                         next
                     end 
                     next
@@ -51,7 +51,7 @@ def human_codebreaker(human,computer)
             end
         end
         
-        if human.secret_code.eql?(computer.secret_code)
+        if $human.secret_code.eql?($computer.secret_code)
             puts "would you look at that! You Win!"
             puts "---------------feedback---------------"
             p feedback
@@ -69,22 +69,22 @@ def human_codebreaker(human,computer)
         if stop === true || tries === 12
             puts "You've exhausted all your chances" if tries === 12 
             puts "-------the secret code is--------"
-            p computer.secret_code
+            p $computer.secret_code
             puts "---------------------------------"
             break
         end
     end    
 end
 
-def artificial_codebreaker(human,computer)
+def artificial_codebreaker
     puts "-------------Computer guessing--------------"
     stop = false
     tries = 0
     loop do
         puts "-------------Guess Again!-------------" unless tries === 0
-        computer.guess_secret_code
+        $computer.guess_secret_code
         
-        if computer.secret_code.eql?(human.secret_code)
+        if $computer.secret_code.eql?($human.secret_code)
             puts "OOOoooH! you've been outwitted The AI Wins!"
             # puts "---------------feedback---------------"
             # p feedback
@@ -102,8 +102,8 @@ def artificial_codebreaker(human,computer)
         if stop === true || tries === 12
             puts "You've exhausted all your chances" if tries === 12 
             puts "-------the secret code is--------"
-            p human.secret_code
-            human.secret_code = {}
+            p $human.secret_code
+            $human.secret_code = {}
             puts "---------------------------------"
             break
         end
@@ -121,24 +121,24 @@ while is_valid_input === false
     role = gets.chomp
     case role
     when "codemaker"
-        computer.secret_code = {}
-        human.role = role
-        computer.role = "codebreaker"
-        puts "the computer is the #{computer.role}"
-        human.create_secret_code
-        computer.feedback = human.secret_code
-        artificial_codebreaker(human,computer)
+        $computer.secret_code = {}
+        $human.role = role
+        $computer.role = "codebreaker"
+        puts "the computer is the #{$computer.role}"
+        $human.create_secret_code
+        $computer.feedback = $human.secret_code
+        artificial_codebreaker
         if play_again?
             is_valid_input = false
         else
             is_valid_input = true            
         end
     when "codebreaker"
-        human.role = role
-        computer.role = "codemaker"
-        puts "the computer is the #{computer.role}"
-        computer.create_secret_code
-        human_codebreaker(human,computer)
+        $human.role = role
+        $computer.role = "codemaker"
+        puts "the computer is the #{$computer.role}"
+        $computer.create_secret_code
+        human_codebreaker
         if play_again?
             is_valid_input = false
         else
